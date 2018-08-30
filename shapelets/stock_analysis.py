bd7a11a64c771dff837774d66abc166c39b335e5
Oliver de Bruin <oliverdb@hotmail.co.za>
2018-08-30 15:35:57 +0200

    created a script to analyze the most and least variable stocks

--- shapelets/stock_analysis.py
+++ shapelets/stock_analysis.py
@@ -6,10 +6,41 @@ files = {}
 
 archive_dir = '../data/jse'
 
+def analyze_sets():
+    files = {}
+    sets = {}
+    for _file in os.listdir(archive_dir):
+        path = os.path.join(archive_dir, _file)
+        df = read_csv(path, usecols=[1]) 
+        sets[_file] = df
+        files[_file] = {
+             'variance' : df['Closing (c)'].std(),
+             'mean' : df['Closing (c)'].mean(),
+        }
 
- for _file in os.listdir(archive_dir):
-        print (_file)
-        open_file = readcsv(os.path.join(archive_dir, _file), 
+    # list_files = [(k,v) for k,v in sorted(files.items(), key=lambda (i,j): (j, i))]
+    list_files = sorted(files, key=lambda x: (files[x]['variance'], files[x]['mean']) , reverse=True)
+    
+    import matplotlib.pyplot as plt
+
+    print ("Top 3 most variable stocks")
+    for i,key in enumerate(list_files[:3]):
+        legend = key.split('-')[1]    
+        plt.plot(sets[key].values, label='%d MOST variable %s: %.2f' % (i+1, legend, files[key]['variance']))    
+        print ('%s:' % key, files[key])
+    
+    print ("\n\nTop 3 LEAST variable stocks (most stable?)")    
+    for i,key in enumerate(list_files[-3:]):
+        legend = key.split('-')[1]
+        plt.plot(sets[key].values, label='%d LEAST var %s: %.2f' % (3-i, legend, files[key]['variance']))
+        print ('%s:' % key, files[key])
+    
+    # most = list_files[0]
+    # least = list_files[-1]
+    plt.legend(loc='best')
+    plt.show()
+    
+
 
 
 
@@ -22,9 +53,7 @@ def remove_below_n_lines(n):
         if lines < n:
             os.remove(os.path.abspath(os.path.join(archive_dir, _file)))
 
-    #     if not _file.endswith('.csv'):
-    #         continue
-        
-    #     files[os.path.join(archive_dir, _file)] = {
-    #         'variance' : np.var()
-    #     }
+
+
+if __name__ == '__main__':
+    analyze_sets()
